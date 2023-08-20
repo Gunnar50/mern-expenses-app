75e5c3043395cd472a41d201c0d7e679e64208ee
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-20 12:48:41 +0100

    created room model

---
+++ server/src/models/Rooms.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+
+const RoomSchema = new mongoose.Schema({
+    code: {type: String, required:true},
+    title: {type: String, required:true},
+    users: [{type:mongoose.Schema.Types.ObjectId, ref: "users", }],
+    expenses: [{type:mongoose.Schema.Types.ObjectId, ref: "expenses"}],
+    total_amount: {type:Number, default: 0},
+    created_at: {type:Date, default: Date.now()}
+});
+
+export const RoomModel = mongoose.model("rooms", RoomSchema) 
