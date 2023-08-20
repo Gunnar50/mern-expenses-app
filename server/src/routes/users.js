bddd53963d93a408ced0a3a1efa6f1e9a13961a6
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-20 12:49:19 +0100

    created user routes

---
+++ server/src/routes/users.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { registerUser, loginUser, verifyToken, getCurrentUser } from "../controllers/userController.js";
+import {authMiddleware} from "../middleware/authMiddleware.js";
+
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/user/:id", getCurrentUser);
+router.get("/verify-token", authMiddleware, verifyToken); // applied the middleware before the verifyToken controller
+
+export {router as userRouter}
+
+
