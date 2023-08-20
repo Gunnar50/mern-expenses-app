ccc474eab200502a3b6de0c1049095ea1356916f
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-20 12:48:34 +0100

    created expense model

---
+++ server/src/models/Expenses.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+const ExpenseSchema = new mongoose.Schema({
+    title: {type:String, required:true},
+    price: {type:mongoose.Schema.Types.Decimal128, required:true},
+    paid_by: {type:mongoose.Schema.Types.ObjectId, ref: "users", required:true},
+    participants: [{type:mongoose.Schema.Types.ObjectId, ref: "users"}],
+    created_at: {type:Date, default: Date.now()}
+});
+
+export const ExpenseModel = mongoose.model("expenses", ExpenseSchema) // "users" is the name of the collection 
