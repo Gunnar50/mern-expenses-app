306b62a80badb96ae3ea16a6bd1895d4221abcf3
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-20 12:47:37 +0100

    created routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import { userRouter } from "./routes/users.js";
-// import { recipesRouter } from "./routes/recipes.js";
+import { userRouter } from "./routes/users.js";
+import { expenseRouter } from "./routes/expenses.js";
+import { roomRouter } from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());  // when sending data from the frontend it will convert into json automatically
 app.use(cors());
-// app.use("/auth", userRouter);
-// app.use("/recipes", recipesRouter);
+app.use("/auth", userRouter);
+app.use("/expense", expenseRouter);
+app.use("/room", roomRouter);
 
 mongoose.connect(process.env.MONGO_URI);
 
