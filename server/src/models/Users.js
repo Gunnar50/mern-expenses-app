ca7e5f3b286380c2b5f97be1dad2418b19014a48
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-20 12:48:48 +0100

    created user model

---
+++ server/src/models/Users.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema({
+    name: {type:String, required:true, unique: true},
+    email: {type:String, required:true, unique: true},
+    password: {type:String, required:true},
+    cash_amount: {type:String, required:true, default:0},
+    created_at: {type:Date, default: Date.now()}
+});
+
+export const UserModel = mongoose.model("users", UserSchema) // "users" is the name of the collection 
