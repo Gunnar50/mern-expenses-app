bc678305f0c361324886a864b2fd500024bffdef
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-20 08:25:22 +0100

    Initial commit

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// import { userRouter } from "./routes/users.js";
+// import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());  // when sending data from the frontend it will convert into json automatically
+app.use(cors());
+// app.use("/auth", userRouter);
+// app.use("/recipes", recipesRouter);
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.listen(process.env.PORT, () => console.log("SERVER STARTED", process.env.PORT));
