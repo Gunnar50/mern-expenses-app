eccb637ac7d9399e9463f06137f7146c8d069bca
Gunnar <gustavo.passarella@hotmail.com>
2023-08-21 18:56:54 +0100

    update routes

--- server/src/routes/expenses.js
+++ server/src/routes/expenses.js
@@ -6,7 +6,7 @@ import {authMiddleware} from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/add", createExpense); // add a new expense
-router.put("/update", updateExpense); // update a expense
+router.put("/edit/:expenseid", updateExpense); // update a expense
 router.delete("/remove", deleteExpense); // remove a expense
 router.get("/", getAllExpenses); // get a list of expenses in the current room, all of them
 router.get("/:expenseid", getExpenseID); // get a single expense by ID
