7b0250a753d70c2553467dbddf16ca5ba888141c
Gunnar <gustavo.passarella@hotmail.com>
2023-08-23 10:48:11 +0100

    added home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { Home } from './pages/home';
+import { Register } from './pages/register';
+import { Login } from './pages/login';
+import { CreateExpense } from './pages/create-expense';
+import { CreateRoom } from './pages/create-room';
+import { EditExpense } from './pages/edit-expense';
+import { Room } from './pages/room';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home/>} />
+					
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+  }
 
 export default App;
