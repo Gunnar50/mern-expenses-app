666e9ba9d71df3f38248a3ee649e6e17db92ee84
Gunnar <gustavo.passarella@hotmail.com>
2023-08-23 10:48:55 +0100

    added register route

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<Home/>} />
+					<Route path='/register' element={<Register/>} />
+					
 					
 				</Routes>
 			</BrowserRouter>
