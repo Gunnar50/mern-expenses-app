7307a5a72b611044a9a5aa874f06014bacc44e6c
Gunnar <gustavo.passarella@hotmail.com>
2023-08-24 08:26:58 +0100

    added sign up form

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -6,7 +6,17 @@ import axios from "axios";
 export const Register = () => {
 	return (
 		<div>
-			
+			<h1>Sign Up</h1>
+			<form onSubmit={onSubmit}>
+            <h2 className="card-title">{label}</h2>
+            <div className="form-group">
+                <input className="form-control" type="text" id="username" placeholder="Username" onChange={(event) => setUsername(event.target.value)}/>
+            </div>
+            <div className="form-group">
+                <input className="form-control" type="password" id="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
+            </div>
+            <button className="btn btn-primary" type="submit">{label}</button>
+        </form>
 		</div>
 	)
 }
