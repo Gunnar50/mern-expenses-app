0d79cd68d879a63b3ccc98b05deaec91cb820468
Gunnar <gustavo.passarella@hotmail.com>
2023-08-25 15:05:16 +0100

    created nav

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,16 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
-
+import { Link, useNavigate } from 'react-router-dom';
 
 
 export const Home = () => {
 	return (
 		<div>
-			
+			<h1>Share Expenses</h1>
+			<div className="nav">
+				<Link className="nav-button" to="/register">Sign Up</Link>
+				<Link className="nav-button" to="/login">Login</Link>
+			</div>
 		</div>
 	)
 }
