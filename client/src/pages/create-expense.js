cde9973ce7014218c8769f19821616c667450f75
Gunnar <gustavo.passarella@hotmail.com>
2023-08-26 08:54:29 +0100

    create expense page

--- client/src/pages/create-expense.js
+++ client/src/pages/create-expense.js
@@ -1,12 +1,48 @@
 import {useState, useEffect} from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 
 
 export const CreateExpense = () => {
+	const [expense, setExpense] = useState({
+		title: "",
+		price: 0,
+		paid_by: 0,
+		participants: [],
+	});
+    const navigate = useNavigate();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("http://localhost:3001/expense/add", {expense});
+            alert("Expense Created!");
+        } catch (err) {console.error(err);}
+
+    }
+
 	return (
 		<div>
-			
+			<form onSubmit={onSubmit}>
+                <h2 className="card-title">Create Expense</h2>
+                <div className="form-group">
+                    <input className="form-control" type="text" id="title" placeholder="Expense Title" onChange={(event) => setTitle(event.target.value)} required/>
+                </div>
+				<div className="form-group">
+                    <input className="form-control" type="number" id="price" placeholder="Price" onChange={(event) => setTitle(event.target.value)} required/>
+                </div>
+				<div className="form-group">
+                    <select className="form-control" id="paid-by" placeholder="Paid By" onChange={(event) => setTitle(event.target.value)} required>
+						<option>user1</option>
+						<option>user2</option>
+					</select>
+                </div>
+				<div className="form-group">
+                    <input className="form-control" type="checkbox" id="price" placeholder="Price" onChange={(event) => setTitle(event.target.value)} required/>
+                </div>
+                <button className="btn btn-primary" type="submit">Create</button>
+            </form>
 		</div>
 	)
 }
