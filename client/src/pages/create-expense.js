5f8069fabcf8c2dddf21ea214370be84518016fc
Gunnar <gustavo.passarella@hotmail.com>
2023-08-26 09:02:34 +0100

    added get all user function

--- client/src/pages/create-expense.js
+++ client/src/pages/create-expense.js
@@ -13,6 +13,13 @@ export const CreateExpense = () => {
 	});
     const navigate = useNavigate();
 
+	const getUsersInRoom = async() => {
+		try {
+            await axios.post("http://localhost:3001/expense/add", {expense});
+            alert("Expense Created!");
+        } catch (err) {console.error(err);}
+	};
+
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
@@ -26,18 +33,23 @@ export const CreateExpense = () => {
 		<div>
 			<form onSubmit={onSubmit}>
                 <h2 className="card-title">Create Expense</h2>
+
                 <div className="form-group">
                     <input className="form-control" type="text" id="title" placeholder="Expense Title" onChange={(event) => setExpense(event.target.value)} required/>
                 </div>
+
 				<div className="form-group">
                     <input className="form-control" type="number" id="price" placeholder="Price" onChange={(event) => setExpense(event.target.value)} required/>
                 </div>
+
 				<div className="form-group">
                     <select className="form-control" id="paid-by" placeholder="Paid By" onChange={(event) => setExpense(event.target.value)} required>
 						<option>user1</option>
 						<option>user2</option>
 					</select>
                 </div>
+
+				{/* need to add the list of participants */}
 				
                 <button className="btn btn-primary" type="submit">Create</button>
             </form>
