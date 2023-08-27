6f9aa9e272d95813d981490e8e97e4d710c2d745
Gunnar <gustavo.passarella@hotmail.com>
2023-08-27 12:18:15 +0100

    started room page

--- client/src/pages/room.js
+++ client/src/pages/room.js
@@ -1,12 +1,33 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
 
 
 export const Room = () => {
+	const roomID = useParams();
+	const [room, setRoom] = useState(null);
+
+	useEffect(() => {
+		const getRoom = () => {
+
+		}
+
+		getRoom();
+	}, [])
+
+	if (!room) return null;
+	
 	return (
 		<div>
-			
+			<h1>Room Title</h1>
+			<div className="navigation">
+				<Link>Balance</Link>
+				<button>Add Expense</button>
+			</div>
+			<div className="expenses-list">
+				
+			</div>
 		</div>
 	)
 }
